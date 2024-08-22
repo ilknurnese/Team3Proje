17396de83814192cfd413fde54e0dbe43f8aec56
nese <ilknurnese@gmail.com>
2024-08-22 19:36:29 +0100

    Task 1 kodları

---
+++ src/Gun01/Nese.java
@@ -0,0 +1,12 @@
+package Gun01;
+
+public class Nese {
+    public static void main(String[] args) {
+
+        System.out.println("Neşe'den Merhaba!");
+        System.out.println("Nasılsın?");
+
+        System.out.println("Task 1 yapıldı");
+
+    }
+}
